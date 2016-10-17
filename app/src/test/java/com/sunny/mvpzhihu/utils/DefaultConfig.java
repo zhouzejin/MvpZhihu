9234411a76263fd675fd522dcc3c4349d9f150a6
Zhou Zejin <zjzhou1@chinaums.com>
2016-10-17 15:31:21 +0800

    delete test layer

--- app/src/test/java/com/sunny/mvpzhihu/utils/DefaultConfig.java
+++
@@ -1,9 +0,0 @@
-package com.sunny.mvpzhihu.utils;
-
-/**
- * Robolectric default config properties
- */
-public class DefaultConfig {
-    //The api level that Roboelectric will use to run the unit tests
-    public static final int EMULATE_SDK = 23;
-}
