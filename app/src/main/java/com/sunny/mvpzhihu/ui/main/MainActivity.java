31e442cfb1f08067055206c42fed0d30406cc286
ZhouZejin <zhouzejine@126.com>
2017-04-19 20:32:00 +0800

    develop main toolbar UI

--- app/src/main/java/com/sunny/mvpzhihu/ui/main/MainActivity.java
+++ app/src/main/java/com/sunny/mvpzhihu/ui/main/MainActivity.java
@@ -1,15 +1,27 @@
 package com.sunny.mvpzhihu.ui.main;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.sunny.mvpzhihu.R;
 import com.sunny.mvpzhihu.ui.base.BaseActivity;
+
+import butterknife.BindView;
 
 /**
  * Main Activity
  * Created by Zhou Zejin on 2016/10/24.
  */
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView mBottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +40,29 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initToolBar() {
-
+        mToolbar.setTitle(R.string.app_name);
+        setSupportActionBar(mToolbar);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_more:
+                Toast.makeText(this, "更多选项", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
