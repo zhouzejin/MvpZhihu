cfb72cc123159fcf1c18c4d04800df4bc55d9c30
ZhouZejin <zhouzejine@126.com>
2017-07-17 10:44:36 +0800

    关于我页面实现

--- app/src/main/java/com/sunny/mvpzhihu/ui/more/MyInfoActivity.java
+++ app/src/main/java/com/sunny/mvpzhihu/ui/more/MyInfoActivity.java
@@ -1,6 +1,5 @@
 package com.sunny.mvpzhihu.ui.more;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -9,20 +8,19 @@ import com.sunny.mvpzhihu.R;
 import com.sunny.mvpzhihu.ui.base.BaseActivity;
 
 import butterknife.BindView;
-import butterknife.OnClick;
 
 /**
- * The type More activity.
- * Created by Zhou Zejin on 2017/7/14.
+ * The type My Info activity.
+ * Created by Zhou Zejin on 2017/7/17.
  */
-public class MoreActivity extends BaseActivity {
+public class MyInfoActivity extends BaseActivity {
 
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
 
     @Override
     public int getLayoutId() {
-        return R.layout.activity_more;
+        return R.layout.activity_my_info;
     }
 
     @Override
@@ -35,27 +33,12 @@ public class MoreActivity extends BaseActivity {
                 finish();
             }
         });
-        getSupportActionBar().setTitle(getString(R.string.action_more));
+        getSupportActionBar().setTitle(getString(R.string.about_me));
     }
 
     @Override
     public void initViews(Bundle savedInstanceState) {
         setSwipeBackEnable(true);
-    }
-
-    @OnClick({R.id.rl_my_info, R.id.tv_feedback, R.id.tv_setting, R.id.tv_about})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.rl_my_info:
-                startActivity(new Intent(this, MyInfoActivity.class));
-                break;
-            case R.id.tv_feedback:
-                break;
-            case R.id.tv_setting:
-                break;
-            case R.id.tv_about:
-                break;
-        }
     }
 
 }
